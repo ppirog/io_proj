c529d8f0fb69e248be92a8eeb2294fd512c50dc2
iamdrzazgowski <iamdrzazgowski@gmail.com>
2024-05-18 10:39:45 +0200

    dziala kod

---
+++ cmake-build-debug/CMakeFiles/io_proj.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Studia/io/io_proj/kod/Kurs.cpp" "CMakeFiles/io_proj.dir/kod/Kurs.cpp.obj" "gcc" "CMakeFiles/io_proj.dir/kod/Kurs.cpp.obj.d"
+  "D:/Studia/io/io_proj/kod/Nauczyciel.cpp" "CMakeFiles/io_proj.dir/kod/Nauczyciel.cpp.obj" "gcc" "CMakeFiles/io_proj.dir/kod/Nauczyciel.cpp.obj.d"
+  "D:/Studia/io/io_proj/kod/Student.cpp" "CMakeFiles/io_proj.dir/kod/Student.cpp.obj" "gcc" "CMakeFiles/io_proj.dir/kod/Student.cpp.obj.d"
+  "D:/Studia/io/io_proj/kod/System.cpp" "CMakeFiles/io_proj.dir/kod/System.cpp.obj" "gcc" "CMakeFiles/io_proj.dir/kod/System.cpp.obj.d"
+  "D:/Studia/io/io_proj/kod/Uzytkownik.cpp" "CMakeFiles/io_proj.dir/kod/Uzytkownik.cpp.obj" "gcc" "CMakeFiles/io_proj.dir/kod/Uzytkownik.cpp.obj.d"
+  "D:/Studia/io/io_proj/kod/Wydzial.cpp" "CMakeFiles/io_proj.dir/kod/Wydzial.cpp.obj" "gcc" "CMakeFiles/io_proj.dir/kod/Wydzial.cpp.obj.d"
+  "D:/Studia/io/io_proj/kod/main.cpp" "CMakeFiles/io_proj.dir/kod/main.cpp.obj" "gcc" "CMakeFiles/io_proj.dir/kod/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/io_proj.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/io_proj.dir/kod/Kurs.cpp.obj"
+  "CMakeFiles/io_proj.dir/kod/Kurs.cpp.obj.d"
+  "CMakeFiles/io_proj.dir/kod/Nauczyciel.cpp.obj"
+  "CMakeFiles/io_proj.dir/kod/Nauczyciel.cpp.obj.d"
+  "CMakeFiles/io_proj.dir/kod/Student.cpp.obj"
+  "CMakeFiles/io_proj.dir/kod/Student.cpp.obj.d"
+  "CMakeFiles/io_proj.dir/kod/System.cpp.obj"
+  "CMakeFiles/io_proj.dir/kod/System.cpp.obj.d"
+  "CMakeFiles/io_proj.dir/kod/Uzytkownik.cpp.obj"
+  "CMakeFiles/io_proj.dir/kod/Uzytkownik.cpp.obj.d"
+  "CMakeFiles/io_proj.dir/kod/Wydzial.cpp.obj"
+  "CMakeFiles/io_proj.dir/kod/Wydzial.cpp.obj.d"
+  "CMakeFiles/io_proj.dir/kod/main.cpp.obj"
+  "CMakeFiles/io_proj.dir/kod/main.cpp.obj.d"
+  "io_proj.exe"
+  "io_proj.exe.manifest"
+  "io_proj.pdb"
+  "libio_proj.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/io_proj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
